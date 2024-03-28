48ba65e342224587950e2b189f46bc2892590aae
Owen Blacker <owen@blacker.me.uk>
2024-03-28 10:47:22 +0000

    Update q3 with more examples

--- chapter03/ch3-q03.js
+++ chapter03/ch3-q03.js
@@ -14,3 +14,6 @@ console.log("f = x => x / 2, low = 0, high = 9", values(x => x / 2, 0, 9));
 console.log("f = x => -x, low = 0, high = 9", values(x => -x, 0, 9));
 console.log("f = x => x * x, low = 0, high = 9", values(x => x * x, 0, 9));
 console.log("f = Math.sqrt, low = 0, high = 9", values(Math.sqrt, 0, 9));
+console.log("f = function(x) { return x*x*x;}, low = 0, high = 9", values(function(x) { return x*x*x;}, 0, 9));
+console.log("f = Math.sqrt, low = 23, high = 45",
+values(Math.sqrt, 23, 45));
